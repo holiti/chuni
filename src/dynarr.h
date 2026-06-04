a88fe7637148759be0679ba51c8d2d44cb63b9f8
Holi_t <salootdayte@gmail.com>
2026-06-04 14:29:55 +0300

    Dynamic templated array

---
+++ src/dynarr.h
@@ -0,0 +1,33 @@
+#ifndef DYNARR
+#define DYNARR
+
+#include <stdlib.h>
+
+#define DYN_STRUCT(TYPE, NAME)                                                 \
+    typedef struct dyn_##NAME                                                  \
+    {                                                                          \
+        size_t len;                                                            \
+        size_t capacity;                                                       \
+        TYPE *arr;                                                             \
+    } dyn_##NAME;                                                              \
+                                                                               \
+    static inline dyn_##NAME *dyn_##NAME##_init()                              \
+    {                                                                          \
+        dyn_##NAME *obj = calloc(1, sizeof(dyn_##NAME));                       \
+        obj->len = 0;                                                          \
+        obj->capacity = 1;                                                     \
+        obj->arr = calloc(obj->len, sizeof(TYPE));                             \
+        return obj;                                                            \
+    }                                                                          \
+    static inline void dyn_##NAME##_push(dyn_##NAME *obj, TYPE val)            \
+    {                                                                          \
+        if (obj->len >= obj->capacity)                                         \
+        {                                                                      \
+            obj->capacity = obj->capacity * 2;                                 \
+            obj->arr = reallocarray(obj->arr, obj->capacity, sizeof(TYPE));    \
+        }                                                                      \
+        obj->arr[obj->len++] = val;                                            \
+        obj->arr[obj->len] = 0;                                                \
+    }
+
+#endif
